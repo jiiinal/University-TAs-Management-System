22cfe397d6637d3e84121c6ffa50f61396fa5f75
krupalspatel <54074757+krupalspatel@users.noreply.github.com>
2022-06-28 19:06:04 +0530

    Update urls.py

--- Main/urls.py
+++ Main/urls.py
@@ -4,6 +4,19 @@ from .views import home
 from .coviews import *
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import TAhome
+from .views import viewCourse
+from .views import StiReq
+from .views import studentHome
+from .views import facultyHome
+from .views import rateTA
+from .views import seeReq
+from .views import assignedCourse
+from .views import submitPref
+from .views import seeList
+from django.conf.urls.static import static
+from django.conf import settings
+from Main import views
 
 urlpatterns = [
     path('home/',home,name='home'),
@@ -30,4 +43,23 @@ urlpatterns = [
     path('cohome/ta/talist2/',talist2,name='talist2'),
     path('cohome/makeannounce/',makeannounce,name='makeannounce'),
     path('cohome/declare/',declare,name='declare'),
+    path('TA/', TAhome, name='TAhome'),
+    path('TA/viewCourse/<str:id>', viewCourse,  name='viewcourse'),
+    path('TA/StipendReq/',StiReq),
+    path('student/', studentHome),
+    path('faculty/', facultyHome),
+    path('student/rateTA', rateTA),
+    path('faculty/seeRequests', seeReq),
+    path('faculty/assignedCourse',assignedCourse),
+    path('faculty/submitPreference',submitPref),
+    path('faculty/seeTheList',seeList)
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+
+
+  
+   
+
+    
